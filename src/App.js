f114d2b3b003daf2d5e5f344a80b6670995e6476
Nekrudzo <130899u@mail.ru>
2025-03-26 12:39:14 +0500

    change mainLogo path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="header">
         <nav className="header-nav">
           <a className="main-logo">
-            <img src="/static/media/mainLogo.png" alt="mainLogo" />
+            <img src="/build/mainLogo.png" alt="mainLogo" />
           </a>
           <ol className="links-list">
             <li className="link">
